078e6d8987accef65ea00c0bd7b42b6ba1b53849
Swapnil Dubey <sdubey14@student.ubc.ca>
2026-05-05 15:02:43 -0700

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = {}
+
+
+        for (a,b) in prerequisites:
+            if a in prereqs:
+                prereqs[a].append(b)
+            else:
+                prereqs[a] = [b]
+        #{0:[1]}
+        
+        #returns true if can take course(no cycles in its prereq tree)
+        
+        def canTake(course):
+            if course in visited:
+                return False
+
+            if course not in prereqs:
+                return True
+
+
+            visited.add(course)
+            
+
+            res = True
+            for pre in prereqs[course]:
+                res = res and canTake(pre)
+            
+            visited.remove(course)
+            return res
+
+        for i in range(numCourses):
+            visited = set()
+            res = canTake(i)
+            if res == False:
+                return False
+        return True
