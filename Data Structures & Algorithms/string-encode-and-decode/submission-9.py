d0421ae13e395db2d3ee32bda289e7839e50b738
Swapnil Dubey <sdubey14@student.ubc.ca>
2026-05-07 13:21:28 -0700

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+
+        for s in strs:
+            res+=str(len(s))+"#"+s
+        return res
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+
+        while i<len(s):
+            currnum = ""
+            currword = ""
+            while s[i]!="#": 
+                currnum+=s[i]
+                i+=1
+            #i is at # now
+            currnum = int(currnum)
+            for _ in range(currnum):
+                i+=1
+                currword+=s[i]
+            res.append(currword)
+
+            #i is at last letter now
+            i+=1
+        return res
+
+
+            
+            
