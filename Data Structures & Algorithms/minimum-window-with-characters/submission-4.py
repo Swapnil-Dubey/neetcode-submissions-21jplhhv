70584d651b2a6efe3729f3f0ecff68ac301be933
Swapnil Dubey <sdubey14@student.ubc.ca>
2026-05-15 11:29:51 -0700

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.py
@@ -0,0 +1,54 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        #shortest substring of s that contains all chars of t
+
+        needmap = {}
+
+        for c in t:
+            if c in needmap:
+                needmap[c]+=1
+            else:
+                needmap[c] = 1
+        
+        need = len(needmap)
+        have = 0
+        havemap = {}
+
+        res = ""
+        reslen = 1001
+
+
+        l = 0
+        r = 0
+
+        while r<=len(s)-1:
+            if s[r] in needmap:
+                if s[r] in havemap:
+                    havemap[s[r]]+=1
+                else:
+                    havemap[s[r]] = 1
+            
+                if havemap[s[r]]==needmap[s[r]]:
+                    have+=1
+            
+
+            while have == need:
+                if r-l+1<reslen:
+                    res = s[l:r+1]
+                    reslen = r-l+1
+                
+                if s[l] in havemap:
+                    havemap[s[l]]-=1
+                    if havemap[s[l]]<needmap[s[l]]:
+                        have-=1
+                l+=1
+            r+=1
+        
+
+        return res
+
+
+        
+
+
+       
